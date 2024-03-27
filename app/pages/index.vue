ba02cc51e76c6fc6382ef3a20a1d7a7de2e20b33
TAT-pprey <165115468+TAT-pprey@users.noreply.github.com>
2024-03-27 11:10:39 +0700

    Localization

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,7 +5,7 @@
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
         <div class="mb-12 xl:mb-0">
-          <h4 v-if="isSignedUp">Thank you - we'll be in touch shortly.</h4>
+          <h4 v-if="isSignedUp"> ขอบคุณสำหรับข้อมูลการติดต่อ </h4>
 
           <form
             v-else
@@ -20,15 +20,15 @@
               class="appearance-none mb-36 bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
               type="text"
               name="email"
-              placeholder="your@email.com"
-              aria-label="Email address"
+              placeholder="อีเมล์ของคุณ@email.com"
+              aria-label="อีเมล์ของคุณ"
             />
 
             <button
               class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
               type="submit"
             >
-              Sign Up
+              สมัครรับข่าวสาร
             </button>
           </form>
         </div>
